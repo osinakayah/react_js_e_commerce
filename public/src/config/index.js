c5e3f156fbd371bd92d5c3e0322427dc801472a9
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2020-01-06 19:49:23 +0100

    changes

--- public/src/config/index.js
+++ public/src/config/index.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:3333';
+export const BASE_URL = 'http://52.243.57.79/ec/';
 export const PER_PAGE_LIMIT = 30;
 
 export const ERROR_END_OF_CATALOGUE = 'End of Catalogue'
